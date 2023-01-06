59478086a8d46f7a466e05d876aebb1fb06e22f7
nathanlafrogne <nathan.lafrogne@gmail.com>
2023-01-06 16:54:35 +0100

    Add a SceneManager... Maybe useless... First version of a connection panel for admin

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,12 @@
 package src.app;
+
 import src.contenu.Article;
 import src.contenu.Avis;
 import src.contenu.Stock;
 import src.gestion.Admin;
 import src.gestion.BDD;
 
-
-public class App{
+public class App {
 
     private BDD bdd;
     private Admin adminConnecte;
@@ -21,12 +21,12 @@ public class App{
     }
 
     public void connexionAdmin(String email, String mdp) {
-        //TODO
-        //Si infos en paramètre correspondent à un admin dans la BDD
-          //this.adminConnecte = new Admin( -Nom correspondant aux infos paramètres- )
-          //Changement de fenètre ayant accès au Stock
-        //Sinon
-          //Message d'erreur, pas de changement de fenètre
+        // TODO
+        // Si infos en paramètre correspondent à un admin dans la BDD
+        // this.adminConnecte = new Admin( -Nom correspondant aux infos paramètres- )
+        // Changement de fenètre ayant accès au Stock
+        // Sinon
+        // Message d'erreur, pas de changement de fenètre
     }
 
     public Admin getAdminConnecte() {
@@ -37,40 +37,41 @@ public class App{
         return !(adminConnecte == null);
     }
 
-    public void creerArticle (String nom, double prix, int quantite, String photo, String description, String nomCategorie) {
+    public void creerArticle(String nom, double prix, int quantite, String photo, String description,
+            String nomCategorie) {
         Article article = new Article(nom, prix, quantite, photo, description, nomCategorie);
         String[] infosNewArticle = article.getInfoArticle();
         boolean articleAlreadyExist = false;
 
-        for (int i=0 ; i<this.stock.getArrayCategorie().size() ; i++) {
-            for (int k=0 ; k<this.stock.getArrayCategorie().get(i).getArticles().size() ; k++) {
+        for (int i = 0; i < this.stock.getArrayCategorie().size(); i++) {
+            for (int k = 0; k < this.stock.getArrayCategorie().get(i).getArticles().size(); k++) {
                 if (this.stock.getArrayCategorie().get(i).getArticles().get(k).getInfoArticle() == infosNewArticle) {
                     articleAlreadyExist = true;
-                    //TODO : Message prévenant de l'échec de l'opération
+                    // TODO : Message prévenant de l'échec de l'opération
                 }
             }
         }
 
         if (!articleAlreadyExist) {
             boolean categorieEstTrouve = false;
-            for (int i=0 ; i<this.stock.getArrayCategorie().size() ; i++) {
+            for (int i = 0; i < this.stock.getArrayCategorie().size(); i++) {
                 if (this.stock.getArrayCategorie().get(i).getNom() == nomCategorie) {
                     this.stock.getArrayCategorie().get(i).getArticles().add(article);
                     categorieEstTrouve = true;
-                    //TODO : Message validant l'opération
+                    // TODO : Message validant l'opération
                 }
             }
 
-            if(!categorieEstTrouve) {
-                //TODO : Message prévenant de l'échec de l'opération | Variante
+            if (!categorieEstTrouve) {
+                // TODO : Message prévenant de l'échec de l'opération | Variante
             }
         }
     }
 
     public void supprimerArticle(Article article) {
         boolean estTrouve = false;
-        for (int i=0 ; i<this.stock.getArrayCategorie().size() ; i++) {
-            for (int k=0 ; k<this.stock.getArrayCategorie().get(i).getArticles().size() ; k++) {
+        for (int i = 0; i < this.stock.getArrayCategorie().size(); i++) {
+            for (int k = 0; k < this.stock.getArrayCategorie().get(i).getArticles().size(); k++) {
                 if (this.stock.getArrayCategorie().get(i).getArticles().get(k) == article) {
                     this.stock.getArrayCategorie().get(i).getArticles().remove(k);
                     estTrouve = true;
@@ -79,15 +80,14 @@ public class App{
         }
 
         if (estTrouve) {
-            //TODO : Message validant l'opération
-        }
-        else {
-            //TODO : Message prévenant de l'échec de l'opération
+            // TODO : Message validant l'opération
+        } else {
+            // TODO : Message prévenant de l'échec de l'opération
         }
     }
 
     public void modifierArticle(Article article) {
-        //TODO : Affichage qui demande ce qu'il veut changer, par exemple le nom
+        // TODO : Affichage qui demande ce qu'il veut changer, par exemple le nom
         article.setNom("Test");
     }
 
@@ -97,22 +97,21 @@ public class App{
     }
 
     public void creerAdmin(String nom, String email, String mdp) {
-        //TODO : Ajoutez nouvel Admin sur BDD
+        // TODO : Ajoutez nouvel Admin sur BDD
     }
 
     public void creerCategorie(String nom) {
         boolean categorieAlreadyExist = false;
-        for (int i=0 ; i<this.stock.getArrayCategorie().size() ; i++) {
+        for (int i = 0; i < this.stock.getArrayCategorie().size(); i++) {
             if (this.stock.getArrayCategorie().get(i).getNom() == nom) {
                 categorieAlreadyExist = true;
             }
         }
         if (!categorieAlreadyExist) {
             this.stock.creerCategorie(nom);
-            //TODO : Message validant l'opération
-        }
-        else {
-            //TODO : Message prévenant de l'échec de l'opération
+            // TODO : Message validant l'opération
+        } else {
+            // TODO : Message prévenant de l'échec de l'opération
         }
     }
 
--- src/ihm/Window.java
+++ src/ihm/Window.java
@@ -8,37 +8,52 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class Window extends Application {
 
     public static final int LARGEUR_FENETRE = 400;
-    public static final int HAUTEUR_FENETRE = 600;
+    public static final int HAUTEUR_FENETRE = 400;
+    public static final int INPUT_WIDTH = 150;
+    public static final String TITRE_APPLICATION = "Gestion du site";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // TODO Auto-generated method stub
-        Button btn1 = new Button("Say, Hello World");
-        Label labelConnexion = new Label("Connexion");
-        TextField shownTF = new TextField();
-        shownTF.setMaxWidth(150);
-        shownTF.setTranslateX(10);
-        shownTF.setTranslateY(100);
 
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
+        Button btnConnect = new Button("Connect");
+        btnConnect.setTranslateY(70);
 
+        Label labelConnexion = new Label("Connect to the admin account");
+        labelConnexion.setTranslateY(-60);
+        labelConnexion.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+
+        TextField mailInput = new TextField();
+        mailInput.setPromptText("Email");
+        mailInput.setMaxWidth(INPUT_WIDTH);
+        mailInput.setTranslateY(-15);
+
+        TextField mdpInput = new TextField();
+        mdpInput.setPromptText("Password");
+        mdpInput.setMaxWidth(INPUT_WIDTH);
+        mdpInput.setTranslateY(15);
+
+        btnConnect.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                System.out.println("hello world");
+                System.out.println("Trying to connect...");
+                // TODO : Check if email and password are right, then switch scene
             }
         });
-        Pane root = new Pane();
-        root.getChildren().add(btn1);
-        root.getChildren().add(shownTF);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btnConnect);
+        root.getChildren().add(mailInput);
+        root.getChildren().add(mdpInput);
         root.getChildren().add(labelConnexion);
         Scene scene = new Scene(root, HAUTEUR_FENETRE, LARGEUR_FENETRE);
-        primaryStage.setTitle("First JavaFX Application");
+        primaryStage.setTitle(TITRE_APPLICATION);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
